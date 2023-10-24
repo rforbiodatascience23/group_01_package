18b1141184e3f86681aeb252d1c94d296ac7970d
qbechv <kimbech99@gmail.com>
2023-10-24 10:54:16 +0200

    nothing special

--- tests/testthat/test-codon_split.R
+++ tests/testthat/test-codon_split.R
@@ -1,3 +1,3 @@
 test_that("splitting codons from sequence", {
-  expect_equal(codon_split("ATCGATCGTGTACGTAGCTAGTCAGTA", start = 1), c("ATC","GAT","CGT","GTA","CGT","AGC","TAG","TCA","GTA"))
+  expect_equal(codon_split("AUCGATCGTGTACGTAGCTAGTCAGTA", start = 1), c("AUC","GAT","CGT","GTA","CGT","AGC","TAG","TCA","GTA"))
 })
